52b3657d47afec831f5471c0e6a1fe61fdc8b809
Unity Blaster <vedantsrivastava29@gmail.com>
2023-03-03 18:43:41 +0530

    New Commit

--- js/Home.js
+++ js/Home.js
@@ -2,6 +2,6 @@ window.alert('Hello & Welcome!');
 showName()
 
 function showName(){
-    var nName= prompt("What's Your Name?");
-    document.getElementById("Name").innerText = "Hi " + nName + "!";
+    var nName=" " + prompt("What's Your Name?");
+    document.getElementById("Name").innerText = "Hi" + nName + "!";
 }
