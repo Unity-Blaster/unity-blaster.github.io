1abf9ca2a3fcc9aa86d5e3b8e03debed4ed58c2f
Unity Blaster <121755799+Unity-Blaster@users.noreply.github.com>
2023-05-08 21:47:17 +0530

    Overhauled Test site w/ good and new test scrool anim

---
+++ js/Test/app.js
@@ -0,0 +1,26 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show2');
+    } else {
+      entry.target.classList.remove('show2');
+    }
+  });
+});
+const hidden2Elements = document.querySelectorAll(".hidden2");
+hidden2Elements.forEach((el) => observer2.observe(el));
