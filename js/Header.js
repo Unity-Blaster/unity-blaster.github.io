35ef5d8fbf9509b3b8da828ade1f1eccb08d7cc8
Unity Blaster <121755799+Unity-Blaster@users.noreply.github.com>
2023-07-09 16:46:21 +0530

    change blur and add header.js capability, seperated inton nameGame.js as well and added scrool buton using js

--- js/Header.js
+++ js/Header.js
@@ -1,26 +1,3 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-document.querySelector("h3").onmouseover = event => {
-  let iterations = 0;
-  
-  const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText.split("")
-      .map((letter,index) => {
-        if(index < iterations) {
-          return event.target.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iterations >= event.target.dataset.value.length){
-      clearInterval(interval);
-    } 
-    
-    iterations += 1/2;
-  }, 24);
-}
 
 // Get the div with the "box" class
     var he1 = document.getElementById("he1");
