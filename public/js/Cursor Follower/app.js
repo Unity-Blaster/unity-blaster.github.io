d87196582951b379bb492dc7eb21249dacca9435
Unity blaster <121755799+Unity-Blaster@users.noreply.github.com>
2023-09-15 21:24:52 +0000

    added the main thing, just transitions left...

---
+++ public/js/Cursor Follower/app.js
@@ -0,0 +1,27 @@
+document.onmousemove = mouseCoordinates;
+document.onmouseleave = mouseExit;
+
+function mouseCoordinates(event){
+	follow1 = document.getElementById("follow1");
+	follow2 = document.getElementById("follow2");
+
+	var xPos= event.clientX;
+	var yPos= event.clientY;
+	
+	follow1.style.top = parseFloat(yPos) + "px";
+	follow2.style.left = parseFloat(xPos) + "px";
+	follow1.style.opacity = "1";
+	follow2.style.opacity = "1";
+
+	// console.log(follow1.style.top);
+	// console.log(follow2.style.left);
+}
+
+function mouseExit(event) {
+	follow1 = document.getElementById("follow1");
+	follow2 = document.getElementById("follow2");
+	follow1.style.opacity = "0";
+	follow2.style.opacity = "0";
+	follow1.style.transition = "mouseExit 0.5s ease-out";
+	follow2.style.transition = "mouseExit 0.5s ease-out";
+}
