32bdf8cedaa50e743cd4e89e5379777b7a666c10
Unity blaster <121755799+Unity-Blaster@users.noreply.github.com>
2023-09-16 03:50:08 +0000

    the idk save

--- public/js/Cursor Follower/app.js
+++ public/js/Cursor Follower/app.js
@@ -1,5 +1,5 @@
 document.onmousemove = mouseCoordinates;
-document.onmouseleave = mouseExit;
+// document.onmouseleave = mouseExit;
 
 function mouseCoordinates(event){
 	follow1 = document.getElementById("follow1");
@@ -8,13 +8,15 @@ function mouseCoordinates(event){
 	var xPos= event.clientX;
 	var yPos= event.clientY;
 	
-	follow1.style.top = parseFloat(yPos) + "px";
-	follow2.style.left = parseFloat(xPos) + "px";
+	follow1.style.top = parseFloat(yPos) - 25 + "px";
+	follow2.style.left = parseFloat(xPos) - 25 + "px";
 	follow1.style.opacity = "1";
 	follow2.style.opacity = "1";
 
 	// console.log(follow1.style.top);
 	// console.log(follow2.style.left);
+	follow1.innerHTML = follow1.style.top;
+	follow2.innerHTML = follow2.style.left;
 }
 
 function mouseExit(event) {
