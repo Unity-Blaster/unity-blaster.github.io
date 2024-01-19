2eecc9d5fc76069d47af6b13c8b4733d527a2b10
Unity Blaster <121755799+Unity-Blaster@users.noreply.github.com>
2024-01-19 18:48:02 +0530

    fixed site (prev only home was fixed) aka proper linkage and stuff

--- public/js/TestSite/CursorFollower.js
+++ public/js/TestSite/CursorFollower.js
@@ -1,10 +1,11 @@
-const follow = document.getElementById("follow");
-document.addEventListener("pointermove", (event) => {
-  const { clientX, clientY } = event;
-  if (follow) {
-    follow.style.left = clientX + "px";
-    follow.style.top = clientY + "px";
-    follow.style.animationDuration = "3000";
-    follow.style.animationFillMode = "forwards";
-  }
-});
+setTimeout(() => {
+    document.querySelector('body').onmousemove = e => {
+        for (const follow of document.getElementsByClassName("follow")) {
+            const rect = follow.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
+            follow.style.setProperty("--mouse-x", `${x}px`);
+            follow.style.setProperty("--mouse-y", `${y}px`);
+        };
+    };
+}, 100);
